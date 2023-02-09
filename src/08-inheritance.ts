675f121d6f25006fde2e6cd256177d45d23f63ab
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2023-02-09 01:45:39 -0500

    Class 9: learn how inheritance works in TS

---
+++ src/08-inheritance.ts
@@ -0,0 +1,49 @@
+export class Animal {
+  constructor(
+    public name: string,
+  ) {}
+
+  move() {
+    console.log('Moving along!');
+  }
+
+  gretting() {
+    return `Hello, I'm ${this.name}`
+  }
+}
+
+export class Dog extends Animal {
+  constructor(
+    name: string,
+    private _owner: string
+  ) {
+    super(name);
+  }
+
+  get owner() {
+    return this._owner;
+  }
+
+  gretting(): string {
+    return `Hello, I'm ${this.name} and I'm a doggy`
+  }
+
+  woof(times: number) {
+    for (let index = 0; index < times; index++) {
+      console.log('woof');
+    }
+  }
+}
+
+const fifi = new Animal('fifi');
+
+fifi.move(); // Moving along!
+console.log(fifi.gretting()); // Hello, I'm fifi
+
+const chase = new Dog('chase', 'nico');
+
+chase.move(); // Moving along!
+console.log(chase.gretting()); // Hello, I'm chase and I'm a doggy
+chase.woof(5); // woof X5
+console.log(chase.name); // chase
+console.log(chase.owner); // nico
