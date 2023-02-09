43eab5e668e8e5f3c59c84ac5bdda1a76c7c2c8f
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2023-02-09 01:11:25 -0500

    Class 8: learn how to use setters in TS

--- src/07-setters.ts
+++ src/07-setters.ts
@@ -1,45 +1,41 @@
 export class MyDate {
   constructor(
     private year: number,
-    private month: number,
+    private _month: number,
     private _day: number
   ) {}
 
-  /* in order to create getters in TS, we have to use underscore to change the name of our actual
-  variable inside the constructor and refactor our code because we need to differenciate both
-  the private value and the getter that returns that private value. Also, by definition, all
-  getters must return something */
   get day() {
     return this.addPadding(this._day);
   }
 
   public add(amount: number, type: 'days' | 'months' | 'years'): void {
-    const prevMonth = this.month;
+    const prevMonth = this._month;
     switch (type) {
       case 'days':
         if (amount >= 30 || this._day + amount > 30) {
           const accum = this._day + amount;
           const monthsToAdd = Math.floor(accum / 30);
           this._day = accum - (30 * monthsToAdd);
-          this.month += monthsToAdd;
+          this._month += monthsToAdd;
 
-          if (this.month >= 12) {
-            const yearsToAdd = Math.floor(this.month / 12);
+          if (this._month >= 12) {
+            const yearsToAdd = Math.floor(this._month / 12);
             this.year += yearsToAdd;
-            this.month = prevMonth + (monthsToAdd - 12 * yearsToAdd);
+            this._month = prevMonth + (monthsToAdd - 12 * yearsToAdd);
           }
         } else {
           this._day += amount
         }
         break;
         case 'months':
-          if (this.month + amount >= 12) {
-            const accum = this.month + amount;
+          if (this._month + amount >= 12) {
+            const accum = this._month + amount;
             const yearsToAdd = Math.floor(accum / 12);
             this.year += yearsToAdd;
-            this.month = prevMonth + (accum - 12 * yearsToAdd);
+            this._month = prevMonth + (accum - 12 * yearsToAdd);
           } else {
-            this.month += amount
+            this._month += amount
           }
         break;
       case 'years':
@@ -54,33 +50,47 @@ export class MyDate {
 
   getFormatedDate(): string {
     const day = this.addPadding(this._day);
-    const month = this.addPadding(this.month);
+    const month = this.addPadding(this._month);
     return `${day}/${month}/${this.year}`
   }
 
   get(type: 'days' | 'months' | 'years') {
     switch(type){
       case 'days':
-        return this._day;
+        return this.addPadding(this._day);
       case 'months':
-        return this.month;
+        return this.addPadding(this._month);
       case 'years':
         return this.year;
     }
   }
 
-  /* getters are also useful when we want to do some internal logic inside our class but we also
-  want to make the result accesible */
   get isLeapYear() {
-    /* a leap year will always be divisible by 4 */
     if (this.year % 4 === 0) {
-      /* except when they're both divisible by 100 and non divisible by 400 */
       if (this.year % 100 === 0 && this.year % 400 !== 0) {
         return false;
       }
       return true;
     }
     return false
+  }
+
+  /* in contrast with getters, setters are strictly void type functions, meaning that they will
+  never return something. Instead, setters are used to validate and change the class' property
+  based on user input or interaction, that's also why setters must have at least one parameter */
+  set month(value: number) {
+    try {
+      if (value < 1 || value > 12) {
+        throw new Error('You entered an incorrect value. Remember that numeric months must be between 1 and 12');
+      }
+      this._month = value;
+    } catch (e) {
+      console.error((e as Error).message);
+    }
+  }
+
+  get month() {
+    return this._month;
   }
 }
 
@@ -89,10 +99,15 @@ const myDate = new MyDate(2020, 1, 9);
 console.log(myDate);
 console.log(myDate.getFormatedDate()); // 09/01/2020
 
-console.log(myDate.get('days')); // 9
-console.log(myDate.get('months')); // 1
+console.log(myDate.get('days')); // 09
+console.log(myDate.get('months')); // 01
 console.log(myDate.get('years')); // 2020
 
-/* as usual with a getter, it works like a property, instead of a method */
 console.log(myDate.day); // 09
 console.log(myDate.isLeapYear); // true
+
+myDate.month = 11;
+console.log(myDate.month); // 11
+myDate.month = 100;
+// You entered an incorrect value. Remember that numeric months must be between 1 and 12 
+console.log(myDate.month); // 11
